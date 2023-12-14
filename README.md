5185791aa32e3d85c4023ec3324932f50fdb80d2
lioneljuru <lioneljuru@gmail.com>
2023-12-14 14:53:53 +0200

    monty

---
+++ README.md
@@ -0,0 +1,2 @@
+Monty repositrory is a repo whith a monty language that is
+compiled into monty byte codes
---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * f_nop - nothing
+ * 2head: stack head
+ * @counter: line_number
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
